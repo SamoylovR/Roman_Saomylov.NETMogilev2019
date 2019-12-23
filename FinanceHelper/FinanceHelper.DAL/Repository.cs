26865b9f5b46c1b82222c29c2b491de847108475
Rom@ <dlyaacountov@gmail.com>
2019-12-23 17:21:41 +0300

    Removed commented JsonRepository interface instance

--- FinanceHelper/FinanceHelper.DAL/Repository.cs
+++ FinanceHelper/FinanceHelper.DAL/Repository.cs
@@ -8,35 +8,29 @@ namespace FinanceHelper.DAL
     public class Repository : IRepository<Operation>
     {
         private readonly IOperationContext db;
-        //private readonly IJsonRepository jsonRepository;
 
         public Repository(IOperationContext db)
         { 
             this.db = db;
-            //this.jsonRepository = jsonRepository;
         }
 
         public void AddNewOperation(Operation operation)
         {
-            //jsonRepository.AddNewOperation(operation);
             db.Operations.Add(operation);
         }
 
         public IEnumerable<Operation> GetIncome()
         {
-            //return jsonRepository.GetIncome();
             return db.Operations?.Where(op => op.Sum > 0);
         }
 
         public IEnumerable<Operation> GetCosts()
         {
-            //return jsonRepository.GetCosts();
             return db.Operations?.Where(op => op.Sum < 0);
         }
 
         public void ClearOperationData()
         {
-            //jsonRepository.ClearOperationData();
             db.Operations.Clear();
         }
     }
