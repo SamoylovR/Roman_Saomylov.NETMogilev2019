63d0d0d50b303d0bfc8ac157560861290a6fd9c7
Rom@ <dlyaacountov@gmail.com>
2020-01-02 16:25:58 +0300

    Added db project

--- FinanceHelper/FinanceHelper.DALdb/Repository.cs
+++ FinanceHelper/FinanceHelper.DALdb/Repository.cs
@@ -1,11 +1,33 @@
 ﻿using FinanceHelper.Common;
+using FinanceHelper.Common.Entity;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace FinanceHelper.DALdb
 {
-    public class Repository
+    public class Repository : IRepository<Operation>
     {
         SqlConnection connection = new SqlConnection(Config.connectionString);
+
+        public void AddNewOperation(Operation operation)
+        {
+            connection.Open();
+        }
+
+        public void ClearOperationData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Operation> GetCosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Operation> GetIncome()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
